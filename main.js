f66e4876ea92b41624a89306b920f0761aaaf8ba
Muhammad-Mansurov04 <102363363+Muhammad-Mansurov04@users.noreply.github.com>
2022-03-25 15:50:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+$(function(){
+
+
+  $('.top__slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+  });
+
+
+});
+
+  $(function(){
+
+
+    $('.').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+
+});
+
